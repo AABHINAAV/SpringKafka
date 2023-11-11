6d4aee166a33c108a86e64b3362ef8333bfaa58a
aabhinaav <aabhinaavgarg@gmail.com>
2023-11-11 18:14:28 +0530

    Kafka integration in Spring start

---
+++ ProducerClient/src/test/java/com/KafkaSpring/ProducerClient/ProducerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KafkaSpring.ProducerClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
